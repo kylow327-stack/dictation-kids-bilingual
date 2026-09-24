a1d808d83db781292e089428d317800dede00478
kylow327-stack <kylow327@gmail.com>
2026-09-24 10:29:31 +0800

    neutralize broken app.b64 loader for bi-v4

--- app.js
+++ app.js
@@ -1,14 +1,2 @@
-(function(){
-  var n=15, parts=new Array(n), left=n;
-  function go(){
-    if(left) return;
-    (0,eval)(atob(parts.join("")));
-  }
-  for(var i=0;i<n;i++)(function(i){
-    var x=new XMLHttpRequest();
-    x.open("GET","app.b64."+i+".txt",true);
-    x.onload=function(){ parts[i]=x.responseText; left--; go(); };
-    x.onerror=function(){ console.error("b64",i); };
-    x.send();
-  })(i);
-})();
+/* dictation-bi-v4: app inlined in Pages-assembled index.html; stub keeps old script src harmless */
+console.info("dictation-bi-v4 monolith");
